fc34cabf168ea0b735cf6128a4e70d33ed173683
github username <github email address>
2019-11-04 11:37:38 +0000

    done basic methods

--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,23 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+c1 = Cult.new('frantics','london',2010,'more is always better')
+c2 = Cult.new('lunies','new york',2018,'quantity over quality')
+c3 = Cult.new('ravers','shanghai',1998,'the sky is blue')
+
+f1 = Follower.new('john',34,'to be or not to be')
+f2 = Follower.new('james',57,'live or let die')
+f3 = Follower.new('martin',13,'follow your heart')
+
+b1 = BloodOath.new('2013',f1,c1)
+b2 = BloodOath.new('2013',f2,c3)
+
+c1.recruit_follower(f2)
+c1.recruit_follower(f3)
+
+f2.join_cult(c2)
+
 
 binding.pry
 
