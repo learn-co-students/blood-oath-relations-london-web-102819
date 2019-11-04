5c587c32f0680418e9922e8642d396ecab20870a
github username <github email address>
2019-11-04 12:23:06 +0000

    bonus done

--- tools/console.rb
+++ tools/console.rb
@@ -4,21 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
-c1 = Cult.new('frantics','london',2010,'more is always better')
-c2 = Cult.new('lunies','new york',2018,'quantity over quality')
-c3 = Cult.new('ravers','shanghai',1998,'the sky is blue')
+c1 = Cult.new('frantics','london',2010,'more is always better',15)
+c2 = Cult.new('lunies','new york',2018,'quantity over quality',10)
+c3 = Cult.new('ravers','london',1998,'the sky is blue',20)
 
 f1 = Follower.new('john',34,'to be or not to be')
 f2 = Follower.new('james',57,'live or let die')
 f3 = Follower.new('martin',13,'follow your heart')
 
-b1 = BloodOath.new('2013',f1,c1)
+b1 = BloodOath.new('2013',f2,c2)
 b2 = BloodOath.new('2013',f2,c3)
 
 c1.recruit_follower(f2)
 c1.recruit_follower(f3)
 
-f2.join_cult(c2)
+f1.join_cult(c3)
 
 
 binding.pry
